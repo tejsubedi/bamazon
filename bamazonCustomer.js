490ff9f18ea0e9b9dd3c1270185434cee876a008
tejsubedi@gmail.com <tejsubedi@gmail.com>
2019-08-03 21:48:06 -0400

    cli-table added

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -18,3 +18,24 @@ connection.connect(function(err){
     displayProductDetails();
 });
 
+function displayProductDetails() {
+    //Declaration of CLI table with column names
+    var table = new Table ({
+        head: ['item_id', 'product_name', 'department_name', 'price', 'stock_quantity','product_sales'],
+        colWidths: [10, 20, 20, 10, 10, 20]
+    });
+
+    var query = "SELECT * FROM products";
+
+    connection.query(query, function(err, res) {
+      if (err) throw err;
+        
+      //For each record 
+      for(var i=0; i<res.length; i++){
+          table.push([res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity])
+      }
+      // Log all results of the SELECT statement
+      console.log(table.toString());
+      connection.end();
+    });
+  }
