4dc3313c955665862097f88c59177db996349c87
tejsubedi@gmail.com <tejsubedi@gmail.com>
2019-08-03 20:18:57 -0400

    bamazon database schema created

---
+++ bamazonData.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS bamazonDB;
+CREATE DATABASE bamazonDB;
+
+USE bamazonDB;
+
+CREATE TABLE products (
+    item_id INT AUTO_INCREMENT NOT NULL,
+    product_name VARCHAR(50),
+    department_name VARCHAR (45),
+    price DECIMAL(5,2),
+    stock_quantity INT(8),
+    PRIMARY KEY (item_id)
+)
+
