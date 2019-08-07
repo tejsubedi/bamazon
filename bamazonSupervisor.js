8c5db42166391de3af4fc810cd3742a1209bc42d
tejsubedi@gmail.com <tejsubedi@gmail.com>
2019-08-07 19:01:06 -0400

    inquirer function has been added

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -10,3 +10,34 @@ var connection = mysql.createConnection({
     password: "Mysql@123",
     database: "bamazonDB"
 });
+
+// If Error display error else call the runAction function to to provide options to the SuperVisor
+connection.connect(function (err) {
+    if (err) throw err;
+    runAction();
+});
+
+// Function to give the supervisor options
+function runAction() {
+
+    inquirer.prompt({
+        name: "options",
+        type: "list",
+        choices: ["View Product Sales by Department", "Create New Department"]
+
+    }).then(function (answer) {
+        switch (answer.options) {
+            // Display Sales by Department
+            case "View Product Sales by Department":
+                displaydeptSales();
+                break;
+            case "Create New Department":
+                // Add a new Department
+                addDepartment();
+                break;
+
+        }
+    });
+
+
+}
