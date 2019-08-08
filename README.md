281134a814f2f67d9710e2b95a886918b95c78f4
tejsubedi@gmail.com <tejsubedi@gmail.com>
2019-08-07 21:56:33 -0400

    Readme updated

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # bamazon
+
+#Packages added to the file
+**install the npm mysql for database**
+**install the npm inquirer for prompt the message**
+**install the npm cli-table for table head in console**
+**added .gitignore file**
+
+# Purpose of this project
+
+This project is about to get more familiar with the MySql database where we can create our database and update it's data as amazon stock application developed using node.The app will take in orders from customers and delete stock from the store's inventory.  
+
+# How to operate
+**To operate this project it will take some of the commands from command prompt are as follows:**
+* `node bamazonCustomer.js`
+
+* `node bamazonManager.js`
+
+* `node bamazonSupervisor.js`
+
+
+## What each command should do 
+
+**1.`node bamazonCustomer.j` will genereate in terminal**
+
+* Running this application will first display all of the items available for sale. Include the ids, names, and prices of products for sale.
+
+#Image
+![](images/bamazonCustomer-1.png)
+
+
--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -46,9 +46,9 @@ function runAction() {
             head: ['department_id', 'department_Name', 'overhead_costs', 'product_sales', 'total_profit']
             , colWidths: [20, 20, 20, 20, 20]
         });
-        var query = "SELECT dept.department_id as department_id,dept.department_name as department_name,dept.overhead_costs as overhead_costs,prod.product_sales as product_sales,(prod.product_sales-dept.overhead_costs) as total_profit from departments as dept inner join products as prod on prod.department_name = dept.department_name group by dept.department_name order by dept.department_id asc;";
+        var query = "select dept.department_id as department_id,dept.department_name as department_name,dept.overhead_costs as overhead_costs,prod.product_sales as product_sales,(prod.product_sales-dept.overhead_costs) as total_profit from departments as dept inner join products as prod on prod.department_name = dept.department_name group by dept.department_name order by dept.department_id asc;";
         connection.query(query, function (err, res) {
-
+            if(err) throw err;
             for (var i = 0; i < res.length; i++) {
                 table.push([res[i].department_id, res[i].department_name, res[i].overhead_costs, res[i].product_sales, res[i].total_profit]);
             }
@@ -58,5 +58,39 @@ function runAction() {
 
     }
 
+    // Function to add new Department 
+    function addDepartment() {
+        inquirer.prompt([
 
+            {
+                name: "DeptName",
+                type: "input",
+                mesage: "Enter the qunatity you want to increase for the product:"
+            },
+            {
+                name: "Overhead_Cost",
+                type: "input",
+                message: "Enter the overhead cost of the department:"
+            }
+        ]).then(function (newDept) {
+
+            //var newDeptId = newDept.DeptId;
+            var newDeptName = newDept.DeptName;
+            var newoverhead = newDept.Overhead_Cost;
+
+            var query = "insert into departments(department_name,overhead_costs) values('" + newDeptName + "'," + newoverhead + ");"
+            console.log("Query is:" + query);
+            connection.query(query, function (err, res) {
+                if (err) {
+                    console.log(err);
+                }
+                else {
+
+                    console.log("New department added successfully");
+
+
+                }
+            });
+        });
+    }
 }
